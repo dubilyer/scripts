be025115d8eb7e1bce20b7ccd6df4aefa0529e92
dubilyer <edaorik@gmail.com>
2017-01-22 10:23:35 +0200

    api

--- src/apiScript.sh
+++ src/apiScript.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 user='Munir-b:cc2c8e094fec6fe8a77eb45c9656050f'
-#function sendAndWait() {
+function sendAndWait() {
     #run the job and get it's queue id
     queue=`curl -i -X  POST http://ci.gtforge.com:8080/job/$1/buildWithParameters\?$2  --user $user | grep queue | sed -e 's/^Location: //g' | sed -e 's/8080//g' | sed -e 's/[^0-9]//g'` > /dev/null 2>&1
     echo $queue
@@ -19,7 +19,5 @@ user='Munir-b:cc2c8e094fec6fe8a77eb45c9656050f'
         result=`curl -X GET http://ci.gtforge.com:8080/job/$1/lastBuild/api/json --user $user | jq ".result"` > /dev/null 2>&1
         echo $result
     done
-#}
-#sendAndWait $1 $2
-
-#"Automation-scrum-deploy" "ACTION\=tag_scrum\&SCRUM_ORIGIN\=scrum10\&SCRUM_NAME\=scrum19"
+}
+sendAndWait "Automation-scrum-deploy" "ACTION\=tag_scrum\&SCRUM_ORIGIN\=scrum10\&SCRUM_NAME\=scrum19"
