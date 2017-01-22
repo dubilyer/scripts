ac963110e65bf7faadeafafc8b0cbdf616a60d2a
dubilyer <edaorik@gmail.com>
2017-01-22 10:14:21 +0200

    api

--- src/apiScript.sh
+++ src/apiScript.sh
@@ -1,19 +1,25 @@
 #!/usr/bin/sh
-#run the job and get it's queue id
-queue=`curl -i -X  POST http://ci.gtforge.com:8080/job/Automation-scrum-deploy/buildWithParameters\?ACTION\=tag_scrum\&SCRUM_ORIGIN\=scrum10\&SCRUM_NAME\=scrum19  --user livnata:867b08925d839273494b8bd9d17f90309486c83e | grep queue | sed -e 's/^Location: //g' | sed -e 's/8080//g' | sed -e 's/[^0-9]//g'`
-echo $queue
-currentQueue="-1"
-#waiting for job start
-while [ "$currentQueue" != "$queue" ]; do
-    currentQueue=`curl -X GET http://ci.gtforge.com:8080/job/Automation-scrum-deploy/lastBuild/api/json --user livnata:867b08925d839273494b8bd9d17f90309486c83e | jq ".queueId"`
-    echo $currentQueue $queue
-done
-#get job ID
-buildNumber=`curl -X GET http://ci.gtforge.com:8080/job/Automation-scrum-deploy/lastBuild/api/json --user livnata:867b08925d839273494b8bd9d17f90309486c83e | jq ".number"`
-echo $buildNumber
-result="null"
-#waiting for job finish
-while [ "$result" = "null" ]; do
-    result=`curl -X GET http://ci.gtforge.com:8080/job/Automation-scrum-deploy/lastBuild/api/json --user livnata:867b08925d839273494b8bd9d17f90309486c83e | jq ".result"`
-    echo $result
-done
+user='Munir-b:cc2c8e094fec6fe8a77eb45c9656050f'
+#function sendAndWait() {
+    #run the job and get it's queue id
+    queue=`curl -i -X  POST http://ci.gtforge.com:8080/job/$1/buildWithParameters\?$2  --user $user | grep queue | sed -e 's/^Location: //g' | sed -e 's/8080//g' | sed -e 's/[^0-9]//g'` > /dev/null 2>&1
+    echo $queue
+    currentQueue="-1"
+    #waiting for job start
+    while [ "$currentQueue" != "$queue" ]; do
+        currentQueue=`curl -X GET http://ci.gtforge.com:8080/job/$1/lastBuild/api/json --user $user | jq ".queueId"` > /dev/null 2>&1
+        echo $currentQueue $queue
+    done
+    #get job ID
+    buildNumber=`curl -X GET http://ci.gtforge.com:8080/job/$1/lastBuild/api/json --user $user | jq ".number"` > /dev/null 2>&1
+    echo $buildNumber
+    result="null"
+    #waiting for job finish
+    while [ "$result" = "null" ]; do
+        result=`curl -X GET http://ci.gtforge.com:8080/job/$1/lastBuild/api/json --user $user | jq ".result"` > /dev/null 2>&1
+        echo $result
+    done
+#}
+#sendAndWait $1 $2
+
+#"Automation-scrum-deploy" "ACTION\=tag_scrum\&SCRUM_ORIGIN\=scrum10\&SCRUM_NAME\=scrum19"
