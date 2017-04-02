6020d4c234d04000bf6bc9d762639117fd4e0a01
dubilyer <edaorik@gmail.com>
2017-04-02 17:43:00 +0300

    more scripts

--- src/revisions.sh
+++ src/revisions.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
-scrum="scrum18"
-rm revisions.txt
+scrum="scrum8"
+rm revisions{scrum}.txt
 for srv in "il" "ru" "uk" "us"; do
-    printf "${srv}=" >> revisions.txt
-    curl -X GET http://${srv}-scrum18.gett.qa/alive | jq -r '.git_revision' >> revisions.txt
+    printf "${srv}"
+    printf "${srv}=" >> revisions${scrum}.txt
+    curl -X GET http://${srv}-${scrum}.gett.qa/alive | jq -r '.git_revision' >> revisions${scrum}.txt
 done
-for srv in "class-identity" "class-availability" "media" "ordering" "travelpolicy" "trip" "line" "depot" "futureorder" "invitation" "coupon" "charging" "pricing" "commission" "notification" "area" "bookkeeping"; do
-    printf "${srv}=" >> revisions.txt
-    curl -X GET http://${srv}-scrum18.gett.qa/alive | jq -r '.git_revision' >> revisions.txt
+for srv in "pms" "class-identity" "class-availability" "media" "ordering" "travelpolicy" "trip" "line" "depot" "futureorder" "invitation" "coupon" "charging" "pricing" "commission" "notification" "area" "bookkeeping"; do
+    printf "${srv}"
+    printf "${srv}=" >> revisions${scrum}.txt
+    curl -X GET http://${srv}-${scrum}.gett.qa/alive | jq -r '.git_revision' >> revisions${scrum}.txt
 done
 for srv in "preeta" "clientgateway" "location" "supplygateway" "fraud"  "reports"  "rides" "regionidentity" "arm" "b2bgateway"; do
-    printf "${srv}=" >> revisions.txt
-    curl -X GET http://${srv}-scrum18.gett.qa/alive | jq -r '.commit' >> revisions.txt
+    printf "${srv}"
+    printf "${srv}=" >> revisions${scrum}.txt
+    curl -X GET http://${srv}-${scrum}.gett.qa/alive | jq -r '.commit' >> revisions${scrum}.txt
 done
-sed "s/\r/:/g"
+#sed "s/\r/:/g"
 
 #"saw" "victory" "osrm" "invitation" "liveheatmaps" "earnings" "subscription"
